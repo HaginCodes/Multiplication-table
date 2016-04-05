5673e1f4c776e629a780f71036b81993b72d4767
Hagin Onyango <hagincubes@gmail.com>
2016-04-05 14:43:11 -0400

    Updated from Brython Server: April 5, 2016 2:43:14 PM EDT

--- multiplication-table.py
+++ multiplication-table.py
@@ -33,6 +33,4 @@ for x in range(a*b):
         print("{0:>3}".format(x+1))
     else:
         print("{0:>3}".format(x+1), end =" ")
-    for n in range (1,b + 1):   
-        print(n)
-     for indx in range (row, (a +1) 
+    for n in
